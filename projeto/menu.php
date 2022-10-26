cf4a8286ad41db4047e66ff015b4a53aff3f5184
Fernando Henrique <89784479+FerHe05@users.noreply.github.com>
2022-10-25 21:32:08 -0300

    Add files via upload

--- projeto/menu.php
+++ projeto/menu.php
@@ -8,7 +8,6 @@ if (!isset($_SESSION["user"])) //se a sessão "user" n for iniciado
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,11 +16,11 @@ if (!isset($_SESSION["user"])) //se a sessão "user" n for iniciado
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<link href="menu_style.css" rel="stylesheet"/>
-  <title>Document</title>
+  <link href="menu_style.css" rel="stylesheet"/>
+  <title>EnergyCalc</title>
 </head>
 <body>
-<!-- Image and text -->
+  <!-- Image and text -->
 <nav class="navbar navbar-light bg-light">
   <a class="navbar-brand" href="menu.php">
     <img src="imagens/imglogo.jpg" width="40" height="40">
@@ -29,40 +28,29 @@ if (!isset($_SESSION["user"])) //se a sessão "user" n for iniciado
   </a>
 </nav>
 <table class="table">
-  <thead>
+  <thead class="thead-dark">
     <tr>
       <th scope="col">Id</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Valor</th>
+      <th scope="col">Corrente (A)</th>
+      <th scope="col">Tensão (V)</th>
+      <th scope="col">Potência (W)</th>
+      <th scope="col">Valor (R$)</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Corrente</td>
-      <td>Valor A</td>
-
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Tensão</td>
-      <td>Valor V</td>
-     
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Potência</td>
-      <td>Valor W</td>
-      
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td>Total</td>
-      <td>Valor R$</td>
-      
+      <td>50</td>
+      <td>100</td>
+      <td>200</td>
+      <td>300</td>
     </tr>
   </tbody>
 </table>
 
+
+  </tbody>
+</table>
 </body>
 </html>
+
